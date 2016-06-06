1a0ea57ca6015bc4b0877ca270d68e7760de383f
Fahsl, Eric M <eric.m.fahsl@accenture.com>
2016-06-06 16:27:18 -0700

    updated racer data

--- ui/app/scripts/racedata.js
+++ ui/app/scripts/racedata.js
@@ -3,17 +3,27 @@ var raceData = { // jshint ignore:line
         'racename': 'boulder70.3',
         'raceid': '2147483658',
         'bibs': [
-          { 'bib': '482', 'starttime': '07:42:00' },
-          { 'bib': '430', 'starttime': '07:42:00' }
+          { 'bib': '482', 'starttime': '08:12:00' },
+          { 'bib': '430', 'starttime': '07:48:00' }
         ]
       },
       'victoria70.3' : {
         'racename': 'victoria70.3',
         'raceid': '2147483658',
         'bibs': [
-          { 'bib': '482', 'starttime': '07:42:00' },
-          { 'bib': '430', 'starttime': '07:42:00' }
+          { 'bib': '135', 'starttime': '06:10:00' },
+          { 'bib': '718', 'starttime': '06:10:00' },
+          { 'bib': '1226', 'starttime': '06:10:00' },
+          { 'bib': '1060', 'starttime': '06:10:00' },
+          { 'bib': '981', 'starttime': '06:10:00' },
+          { 'bib': '1230', 'starttime': '06:10:00' },
+          { 'bib': '485', 'starttime': '06:10:00' },
+          { 'bib': '588', 'starttime': '06:10:00' },
+          { 'bib': '637', 'starttime': '06:10:00' },
+          { 'bib': '514', 'starttime': '06:10:00' },
+          { 'bib': '4003', 'starttime': '06:10:00' }
+          
         ]
-      }      
+      } 
 
     }; 
